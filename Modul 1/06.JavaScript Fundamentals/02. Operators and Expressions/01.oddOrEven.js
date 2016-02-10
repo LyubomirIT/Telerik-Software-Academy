b8edfb298e1f7deab38e2a0e39bd10afce650527
Semerdzhiev45 <semerdzhiev94@hotmail.com>
2016-02-10 15:20:23 +0200

    added README.md

---
+++ Modul 1/06.JavaScript Fundamentals/02. Operators and Expressions/01.oddOrEven.js
@@ -0,0 +1,15 @@
+function oddOrEven(number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(oddOrEven(14));
+console.log(oddOrEven(1));
+console.log(oddOrEven(12));
+console.log(oddOrEven(2));
+console.log(oddOrEven(10000));
+console.log(oddOrEven(13));
