cb48362d6107aa028c7817b4620d25467df968d8
Semerdzhiev45 <semerdzhiev94@hotmail.com>
2016-02-13 13:21:35 +0200

    Updated Exam

---
+++ Modul 1/01. C#- Part 1/07.Real-Exam-3-Feb-2015-Morning/01.ThreeNumbers/ThreeNumbers.cs
@@ -0,0 +1,39 @@
+﻿using System;
+class ThreeNumbers
+{
+    static void Main()
+    {
+        double A = double.Parse(Console.ReadLine());
+        double B = double.Parse(Console.ReadLine());
+        double C = double.Parse(Console.ReadLine());
+
+        if (A >= B && A >= C)
+        {
+            Console.WriteLine(A);
+        }
+        else if (B >= A && B >= C)
+        {
+            Console.WriteLine(B);
+        }
+        else if (C >= A && C >= B)
+        {
+            Console.WriteLine(C);
+        }
+        if (A <= B && A <= C)
+        {
+            Console.WriteLine(A);
+        }
+        else if (B <= A && B <= C)
+        {
+            Console.WriteLine(B);
+        }
+        else if (C <= A && C <= B)
+        {
+            Console.WriteLine(C);
+        }
+
+        double arithmeticMean = (A + B + C) / 3;
+        Console.WriteLine("{0:F2}", arithmeticMean);
+    }
+}
+
---
+++ Modul 1/01. C#- Part 1/07.Real-Exam-3-Feb-2015-Morning/02.Decoding/Decoding.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Decoding
+{
+    static void Main()
+    {
+        double SALT = double.Parse(Console.ReadLine());
+        double encodeNumber;
+
+        while (SALT < 1 && SALT > 100)
+        {
+            SALT = double.Parse(Console.ReadLine());
+        }
+
+        string text = Console.ReadLine();
+
+        for (int i = 0; i <= text.Length; i++)
+        {
+            if (text[i] == '@')
+            {
+                break;
+            }
+            if (char.IsLetter(text[i]))
+            {
+                encodeNumber = (text[i] * SALT) + 1000;
+            }
+            else if (char.IsNumber(text[i]))
+            {
+                encodeNumber = text[i] + SALT + 500;
+            }
+            else
+            {
+                encodeNumber = text[i] - SALT;
+            }
+            if (i % 2 == 0)
+            {
+                encodeNumber /= 100;
+                Console.WriteLine("{0:F2}", encodeNumber);
+            }
+            else
+            {
+                encodeNumber *= 100;
+                Console.WriteLine(encodeNumber);
+            }
+        }
+    }
+}
+
